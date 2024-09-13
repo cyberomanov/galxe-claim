dd3b00b475ce53cd00c6e8b676fb6ad0d6fb8f06
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2024-09-13 19:01:56 +0400

    fix claim.js

--- claim.js
+++ claim.js
@@ -3,7 +3,7 @@ function delay(time) {
 }
 
 const delayTime = 1000;
-const divSelector = 'div.text-left.flex.gap-3.sm\\:gap-4.items-center.w-full.px-4.sm\\:px-6.py-4.sm\\:py-\\[18px\\].cursor-pointer';
+const divSelector = 'div.w-full.bg-background-lighten2.rounded-lg.border.border-transparent.hover\\:border-white.active\\:border-info-lighten2.relative.transition-all.duration-200.ease-in-out';
 const svgPathSelector = 'path[d="M2.475 10A7.525 7.525 0 0 1 16.14 5.65h-2.89a.6.6 0 1 0 0 1.2h4.25a.6.6 0 0 0 .6-.6V2a.6.6 0 1 0-1.2 0v2.66a8.725 8.725 0 1 0 1.154 8.702.6.6 0 1 0-1.108-.463A7.527 7.527 0 0 1 2.475 10"]';
 
 const clickElementsWithDelay = async () => {
